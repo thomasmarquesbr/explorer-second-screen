61ac4164aa3a3e3c6939de4e1115426431450928
Thomas <msamoht16@gmail.com>
2018-08-16 17:34:21 -0300

    Implementa PaintView

---
+++ app/src/main/java/com/example/thomas/explorador_segunda_tela/CanvasView.java
@@ -0,0 +1,111 @@
+package com.example.thomas.explorador_segunda_tela;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathEffect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CanvasView extends View {
+
+    public int width;
+    public int height;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    Context mContext;
+    private Paint mPaint;
+    private float mX, mY;
+    private static final float TOLERANCE = 5;
+
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.mContext = context;
+
+        // we set a new Path
+        mPath = new Path();
+        // and we set a new Paint with the desired attributes
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(20f);
+        mPaint.setPathEffect(new DashPathEffect(new float[]{50, 20}, 0));
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        // your Canvas will draw onto the defined Bitmap
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // draw the mPath with the mPaint on the canvas when onDraw
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    // when ACTION_DOWN start touch according to the x,y values
+    private void startTouch(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    // when ACTION_MOVE move touch according to the x,y values
+    private void moveTouch(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOLERANCE || dy >= TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    public void clearCanvas() {
+        mPath.reset();
+        invalidate();
+    }
+
+    // when ACTION_UP stop touch
+    private void upTouch() {
+        mPath.lineTo(mX, mY);
+    }
+
+    //override the onTouchEvent
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+}
--- app/src/main/java/com/example/thomas/explorador_segunda_tela/MainActivity.java
+++ app/src/main/java/com/example/thomas/explorador_segunda_tela/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.thomas.explorador_segunda_tela;
 
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    @BindView(R.id.canvas_view) protected CanvasView canvasView;
+
+    private String TAG = "MainActivity";
+    ArrayList<Float> listCoordinateX = new ArrayList<>();
+    ArrayList<Float> listCoordinateY = new ArrayList<>();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        canvasView.setOnTouchListener(this);
+
     }
+
+    @Override
+    protected void onStop() {
+        Log.e("x", listCoordinateX+"");
+        Log.e("y",listCoordinateY+"");
+        super.onStop();
+    }
+
+    public void onClickButton(View v) {
+        canvasView.clearCanvas();
+        startDrawing();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float coordinateX = event.getX();
+        float coordinateY = event.getY();
+        listCoordinateX.add(coordinateX);
+        listCoordinateY.add(coordinateY);
+        return false;
+    }
+
+    public void shootEventTouch(float coordinateX, float coordinateY) {
+        // Obtain MotionEvent object
+        long downTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis() + 100;
+        float x = coordinateX;
+        float y = coordinateY;
+        // List of meta states found here:     developer.android.com/reference/android/view/KeyEvent.html#getMetaState()
+        int metaState = 0;
+        MotionEvent motionEvent = MotionEvent.obtain(
+                downTime,
+                eventTime,
+                MotionEvent.ACTION_MOVE,
+                x,
+                y,
+                metaState
+        );
+        // Dispatch touch event to view
+        canvasView.dispatchTouchEvent(motionEvent);
+//        canvasView.onTouchEvent(motionEvent);
+    }
+
+    public void startDrawing() {
+        CountDownTimer timer = new CountDownTimer(50000, 500) {
+
+            public void onTick(long millisUntilFinished) {
+                Log.e(TAG, String.valueOf((millisUntilFinished/500)));
+                shootEventTouch((millisUntilFinished/500),(millisUntilFinished/500));
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                shootEventTouch(1024f,1024f);
+            }
+        };
+//        timer.start();
+    }
+
 }
