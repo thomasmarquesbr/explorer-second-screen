84913e46e03990086bd3292006fa8efafccca4c4
Thomas <msamoht16@gmail.com>
2018-08-15 23:01:19 -0300

    Adiciona base do projeto

---
+++ app/src/main/java/com/example/thomas/explorador_segunda_tela/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.thomas.explorador_segunda_tela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
